8dfb24a0bdb98b7a3e6af5b7cf35a0bf7c3c54db
Apurv-henkare <70956903+Apurv-henkare@users.noreply.github.com>
2024-03-10 19:37:03 +0530

    Update GDU PICT GDU IIIT K Github Test.java

--- Test.java
+++ Test.java
@@ -1,6 +1,6 @@
 public class Test {
 
     public static void main(String args[]) {
-        System.out.println("GDU PICT !");
+        System.out.println("GDU PICT And GDU IIIT Kottayam");
     }
 }
