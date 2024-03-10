6cc8acbec2d6cafcc0fed3cb49b9a727accaf0d9
Apurv Henkare <apurvhenakare@gmail.com>
2024-03-10 20:29:24 +0530

    Merger resolver

--- Test.java
+++ Test.java
@@ -2,6 +2,6 @@ public class Test {
 
     public static void main(String args[]) {
         System.out.println("GDU PICT And GDU IIIT Kottayam");
-        System.out.println("Hello Bois :)");
+        System.out.println("Hello Girls :)");
     }
 }
