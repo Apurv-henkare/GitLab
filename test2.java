e9d3d6e6f8c6a0dc73fa1cbd9236041ed8af103a
Apurv Henkare <apurvhenakare@gmail.com>
2024-03-15 19:27:50 +0530

    Amnora

--- test2.java
+++ test2.java
@@ -1,7 +1,7 @@
 public class Test {
 
     public static void main(String args[]) {
-        System.out.println("Hey bro this is test2");
+        System.out.println("Amnora bro");
         Object b;
     }
 }
